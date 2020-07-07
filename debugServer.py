190d5cda834774ba6266b3c9e914ea4ccc9c1b75
zhitenev.i <zhitenev.i@kontinent-spb.ru>
2020-07-07 15:14:07 +0300

    Добавлен и протестирован Modbus rtu over TCP.

--- debugServer.py
+++ debugServer.py
@@ -1,6 +1,8 @@
 import threading
 import logging
 from time import sleep
+import socket
+from modbus.modbusCrc import crc16
 
 from flask import Flask, jsonify
 from flask import json
@@ -10,7 +12,6 @@ from flask import request
 class ThreadDevicesNetwork(threading.Thread):
     def __init__(self, device_models, port='80'):
         threading.Thread.__init__(self)
-
         self.__port = port
         self.kill_received = False
         self.device_config = device_models
@@ -66,10 +67,38 @@ class ThreadDevicesNetwork(threading.Thread):
     def run(self):
         logging.info("Web server is started!")
 
-        # while not self.kill_received:
-        try:
-            pass
-        except KeyboardInterrupt:
-            print("Ctrl-c received! Sending kill to threads...ThreadBzuClimate")
-            self.kill_received = True
-            sleep(10)
+        while not self.kill_received:
+            try:
+                with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                    s.bind(('127.0.0.1', 502))
+                    s.listen(1)
+                    conn, addr = s.accept()
+                    print('Connection address:', addr)
+                    data = conn.recv(255)
+                    if data:
+                        msg = ''
+                        for i in data:
+                            msg += "{:x} ".format(i)
+                        print("received data:", msg)
+                        answer = bytearray();
+                        answer.append(0x01)
+                        answer.append(0x03)
+                        count = int(data[5])
+                        answer.append(2*count)
+                        regs = [x for x in range(count)]
+                        for reg in regs:
+                            answer.append(0)
+                            answer.append(reg)
+                        crc = crc16(answer)
+                        answer.append((crc & 0xff))
+                        answer.append((crc & 0xff00) >> 8)
+                        conn.send(answer)
+
+                        msg = ''
+                        for i in answer:
+                            msg += "{:x} ".format(i)
+                        print("sent data:", msg)
+            except KeyboardInterrupt:
+                print("Ctrl-c received! Sending kill to threads...ThreadBzuClimate")
+                self.kill_received = True
+                sleep(10)
